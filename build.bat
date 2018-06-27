17087e33963d35dff91702dba71004feaefdd4bf
famous1622 <Kboorocks@gmx.com>
2018-06-26 22:25:28 -0400

    Actually include libraries now that I checked licensing (oops)

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-pyinstaller --add-data templates;. --add-data static;. fDDMEPlayer.py
+pyinstaller --add-data templates;. --add-data static;. ^
+            --add-binary distlibs;. fDDMEPlayer.py
+move dist\fDDMEPlayer dist\bin
